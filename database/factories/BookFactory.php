c9a9f97959426e769c2dee49bd99418cec8d5239
Gideon Tigelaar <gidtigelaar@gmail.com>
2024-06-10 16:41:18 +0200

    Migrate books table and add BookController

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'author' => $this->faker->name,
+            'isbn' => $this->faker->randomNumber(13),
+            'is_read' => $this->faker->boolean,
+        ];
+    }
+}
